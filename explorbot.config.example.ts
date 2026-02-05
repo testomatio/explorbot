26bf7091469536373d8190825e55c46b034a15e9
nickiovets <indicow@gmail.com>
2026-02-05 14:21:45 +0100

    remove script from config example

--- explorbot.config.example.ts
+++ explorbot.config.example.ts
@@ -117,10 +117,6 @@ const config: ExplorbotConfig = {
       height: 900,
     },
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--disable-gpu'],
-
-    setupScripts: [
-      './browser-scripts/auto-accept-cookies.setup.js',
-    ],
   },
 
   app: {
